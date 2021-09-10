e1a3841fc2b42999f5a3bdd67dc14ca4ced2a3f2
Amfito <elvinas.scirbinskas@gmail.com>
2021-09-10 19:21:21 +0300

    small fix

--- src/main/java/com/webshop/webshop/MainView.java
+++ src/main/java/com/webshop/webshop/MainView.java
@@ -1,22 +1,57 @@
 package com.webshop.webshop;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route
-public class MainView extends VerticalLayout {
+public class MainView extends AppLayout {
+    private H1 viewTitle;
+
     public MainView(){
 
-        add(new H1("Hello"));
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        addToDrawer(createDrawerContent());
 
-        Button button = new Button("Click me", click -> {
-            add(new Span("Clicked"));
-        });
+//        add(new H1("Hello"));
+//
+//        Button button = new Button("Click me", click -> {
+//            add(new Span("Clicked"));
+//        });
+//
+//        add(button);
+    }
 
-        add(button);
+    private Component createHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassName("text-secondary");
+        toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames("m-0", "text-l");
+
+        Header header = new Header(toggle, viewTitle);
+        header.addClassNames("bg-base", "border-b", "border-contrast-10", "box-border", "flex", "h-xl", "items-center",
+                "w-full");
+        return header;
+    }
+
+    private Component createDrawerContent() {
+        H2 appName = new H2("Accenture WebShop");
+        appName.addClassNames("flex", "items-center", "h-xl", "m-0", "px-m", "text-m");
+
+        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName);
+        section.addClassNames("flex", "flex-col", "items-stretch", "max-h-full", "min-h-full");
+        return section;
     }
 }
